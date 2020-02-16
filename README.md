3fa27c2b966e1b98f7063d14379ed4169c066161
Allan Legemaate <alegemaate@gmail.com>
2020-02-15 21:07:00 -0500

    Add webpack, package.json

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # SolitaireJS
+
+Klondike Solitaire JS using Phaser 3
+
+## Demo
+
+[https://adsgames.net/games/solitaire](https://adsgames.net/games/solitaire)
+
+## Running
+
+Simply run
+
+- `npm install`
+- `npm start`
+
+## Building
+
+Simply run
+
+- `npm run build`
---
+++ webpack/base.js
@@ -0,0 +1,42 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: [/\.vert$/, /\.frag$/],
+        use: "raw-loader",
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg|xml|mp3)$/i,
+        use: "file-loader",
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(true),
+    }),
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+  ],
+};
